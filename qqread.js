99b4edc5e760edbabe69f8f0df86524c8700ba2b
summerisjason <qinguangsen3231@163.com>
2020-11-29 14:10:44 +0800

    增加新账号的cookie

--- qqread.js
+++ qqread.js
@@ -54,6 +54,38 @@ const cookiesArr = [
       mpversion: "0.29.4",
     }),
   }
+  {
+    qqreadheaderVal: JSON.stringify({
+      ywsession: "uoz8qmrr7u3ihm4dlohbh1gqtbjytqk1",
+      Cookie:
+          "ywguid=3274287684;ywkey=ywfYxfFlDSZc;platform=android;channel=mqqmina;mpVersion=0.30.0",
+      Connection: "keep-alive",
+      "Content-Type": "application/json",
+      Accept: "*/*",
+      Host: "mqqapi.reader.qq.com",
+      "User-Agent": "QQ/8.4.18.4945 V1_AND_SQ_8.4.18_1558_YYB_D QQ/MiniApp",
+      Referer: "https://appservice.qq.com/1110657249/0.30.0/page-frame.html",
+      "Accept-Language": "zh-cn",
+      "Accept-Encoding": "gzip, deflate, br",
+      mpversion: "0.30.0",
+    }),
+    qqreadtimeurlVal:
+        "https://mqqapi.reader.qq.com/mqq/addReadTimeWithBid?scene=3001&refer=-1&bid=26721432&readTime=5941&read_type=0&conttype=1&read_status=0&chapter_info=%5B%7B%221%22%3A%7B%22readTime%22%3A5941%2C%22pay_status%22%3A0%2C%22is_tail%22%3A0%7D%7D%5D&sp=-1",
+    qqreadtimeheaderVal: JSON.stringify({
+      ywsession: "uoz8qmrr7u3ihm4dlohbh1gqtbjytqk1",
+      Cookie:
+          "ywguid=3274287684;ywkey=ywfYxfFlDSZc;platform=android;channel=mqqmina;mpVersion=0.30.0;qq_ver=8.4.18.4945;os_ver=Android 7.1.2;mpos_ver=1.21.0;platform=android;openid=EC3F1B7CAB5A1532D3BB22EE47133BC4",
+      Connection: "keep-alive",
+      "Content-Type": "application/json",
+      Accept: "*/*",
+      Host: "mqqapi.reader.qq.com",
+      "User-Agent": "QQ/8.4.18.4945 V1_AND_SQ_8.4.18_1558_YYB_D QQ/MiniApp",
+      Referer: "https://appservice.qq.com/1110657249/0.30.0/page-frame.html",
+      "Accept-Language": "zh-cn",
+      "Accept-Encoding": "gzip, deflate, br",
+      mpversion: "0.30.0",
+    }),
+  }
 ]
 
 var tz = "";
