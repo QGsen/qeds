9f35c0a96bd6517d46625b32fcd3431faa9c6685
summerisjason <qinguangsen3231@163.com>
2020-12-01 12:40:35 +0800

    update

--- qqread.js
+++ qqread.js
@@ -150,6 +150,38 @@ const cookiesArr = [
       mpversion: "0.30.0",
     }),
   },
+  {
+    qqreadheaderVal: JSON.stringify({
+      ywsession: "89ns7m3sjkzmpfrlplliir2l6moqjwxh",
+      Cookie:
+          "ywguid=1759055175;ywkey=ywfeN32KkJZi;platform=android;channel=mqqmina;mpVersion=0.30.0",
+      Connection: "keep-alive",
+      "Content-Type": "application/json",
+      Accept: "*/*",
+      Host: "mqqapi.reader.qq.com",
+      "User-Agent": "Mozilla/5.0 (Linux; Android 7.1.2; VOG-AL10 Build/HUAWEIVOG-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 QQ/MiniApp",
+      Referer: "https://appservice.qq.com/1110657249/0.30.0/page-frame.html",
+      "Accept-Language": "zh-cn",
+      "Accept-Encoding": "gzip, deflate, br",
+      mpversion: "0.30.0",
+    }),
+    qqreadtimeurlVal:
+        "https://mqqapi.reader.qq.com/mqq/addReadTimeWithBid?scene=3001&refer=-1&bid=22594978&readTime=2325&read_type=0&conttype=1&read_status=0&chapter_info=%5B%7B%221%22%3A%7B%22readTime%22%3A2325%2C%22pay_status%22%3A0%7D%7D%5D&sp=-1",
+    qqreadtimeheaderVal: JSON.stringify({
+      ywsession: "89ns7m3sjkzmpfrlplliir2l6moqjwxh",
+      Cookie:
+          "ywguid=1759055175;ywkey=ywfeN32KkJZi;platform=android;channel=mqqmina;mpVersion=0.30.0;qq_ver=8.3.9.4635;os_ver=Android 7.1.2;mpos_ver=1.16.0;platform=android;openid=37587F1F4F565B077557692586AF4B49",
+      Connection: "keep-alive",
+      "Content-Type": "application/json",
+      Accept: "*/*",
+      Host: "mqqapi.reader.qq.com",
+      "User-Agent": "Mozilla/5.0 (Linux; Android 7.1.2; VOG-AL10 Build/HUAWEIVOG-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 QQ/MiniApp",
+      Referer: "https://appservice.qq.com/1110657249/0.30.0/page-frame.html",
+      "Accept-Language": "zh-cn",
+      "Accept-Encoding": "gzip, deflate, br",
+      mpversion: "0.30.0",
+    }),
+  },
 ]
 
 var tz = "";
